5adab43ba24a53db1eb1a011355fd699ba477d18
gw9968 <gw9968ans@gmail.com>
2022-10-24 13:58:57 +0200

    V1.2

--- projekt/main.cpp
+++ projekt/main.cpp
@@ -6,10 +6,19 @@ int main(int argc, char** argv) {
 	float a,b,x;
 	cout<<"Podaj A"<<endl;
 	cin >> a;
+	if(a==0)
+	{
+		cout<< "nie ma miejsc zerowych"
+		
+	}
+	else{
+	
 	cout<<"Podaj B"<<endl;
 	cin>> b;
 	x=-(b/a);
 	cout<<"miejsce zerowe tej funkcji to: "<<x<<endl;
+	}
+
 }
 
 
