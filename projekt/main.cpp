d65fa7cbec34f5db453ab3730bd45ca009374b37
gw9968 <gw9968ans@gmail.com>
2022-10-24 13:28:00 +0200

    V1.0

---
+++ projekt/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char** argv) {
+	float a,b,x;
+	cout<<"Podaj A"<<endl;
+	cin >> a;
+	cout<<"Podaj B"<<endl;
+	cin>> b;
+	x=-(b/a);
+	cout<<"miejsce zerowe tej funkcji to: "<<x<<endl;
+}
+
+
